2cf91a2b66e4dfb63d30476e4cf646d8c15d8723
quentin <quentin.malnatti@gmail.com>
2023-09-26 19:23:47 +0200

    read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lib-logger
+Python package which define personal logger. It defines decorators function to log functions 
+
+# deploy new version steps
+### 1. change setup version (setup.py)
+### 2. commit and push changes
+### 3. add local tag (git tag <version number>)
+### 4. push tag (git push origin <version number>)
+
+# Import command
+pip install git+https://github.com/QuentinMalnatti/<lib_repo>.git@v<version_number>#egg=<lib_name>
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='PersonalLogger',
-    url='https://github.com/QuentinMalnatti/lib-logger',
-    author='Quentin Malnatti',
-    packages=['personal_logger'],
-    version='0.1.4',
-    description='Personal logger used to log using decorators',
-    long_description=open('README.md').read(),
+    name="PersonalLogger",
+    url="https://github.com/QuentinMalnatti/lib-logger",
+    author="Quentin Malnatti",
+    packages=["personal_logger"],
+    version="0.1.4",
+    description="Personal logger used to log using decorators",
+    long_description=open("README.md").read(),
 )
